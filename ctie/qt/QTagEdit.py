b5197f9eb87aecc2dda5ef18e1719681881cae29
Buganini Q <buganini@gmail.com>
2018-02-05 14:53:14 +0800

    implement auto height for QTagEdit

--- ctie/qt/QTagEdit.py
+++ ctie/qt/QTagEdit.py
@@ -7,3 +7,24 @@ class QTagEdit(QPlainTextEdit):
         font = QtGui.QFont("Consolas")
         font.setStyleHint(QtGui.QFont.TypeWriter)
         self.setFont(font)
+
+        self.textChanged.connect(self.onTextChanged)
+
+    def setPlainText(self, text):
+        QPlainTextEdit.setPlainText(self, text)
+        self.onTextChanged()
+
+    # https://stackoverflow.com/questions/45028105/get-the-exact-height-of-qtextdocument-in-pixels
+    def onTextChanged(self):
+        doc = self.document()
+        layout = doc.documentLayout()
+        h = 0
+        b = doc.begin()
+        while b != doc.end():
+            h += layout.blockBoundingRect(b).height()
+            b = b.next()
+
+        # magic formula: I do not know why the document margin is already
+        # once included in the height of the last block, and I do not know
+        # why there must be the number 1 at the end... but it works
+        self.setFixedHeight(h + doc.documentMargin() + 2 * self.frameWidth() + 1)
