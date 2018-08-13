5b5c3476acef7ac0791f7eb914dac498f6f8c698
Buganini Q <buganini@gmail.com>
2018-08-14 01:23:16 +0800

    fix data overwrite in UI decorator

--- ctie/qt/helper.py
+++ ctie/qt/helper.py
@@ -2,16 +2,21 @@ from PyQt5 import QtCore
 from functools import partial
 
 class UI(QtCore.QObject):
-    signal = QtCore.pyqtSignal()
+    signal = QtCore.pyqtSignal(int)
     def __init__(self, func):
         QtCore.QObject.__init__(self)
+        self.serial = 0
+        self.data = {}
         self.func = func
         self.signal.connect(self.run)
-    def run(self):
-        self.func(*self.args, **self.kwargs)
+    def run(self, token):
+        args, kwargs = self.data.pop(token)
+        self.func(*args, **kwargs)
     def __call__(self, *args, **kwargs):
-        self.args = args
-        self.kwargs = kwargs
-        self.signal.emit()
+        token = self.serial
+        self.serial = (self.serial + 1) % 65536
+        payload = (args, kwargs)
+        self.data[token] = payload
+        self.signal.emit(token)
     def __get__(self, instance, owner):
         return partial(self.__call__, instance)
